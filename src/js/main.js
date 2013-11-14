165a355c7ab0a63e3ef13bb24b385802fbfdff40
tloureiro <o@o.com>
2013-11-13 19:14:27 -0500

    alpha :)

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+chrome.app.runtime.onLaunched.addListener(function() {
+  chrome.app.window.create('../index.html',
+    {
+      'bounds': {
+        'width': 400,
+        'height': 500
+      }
+    },
+    function(obj) {
+      obj.contentWindow.console.log(':)');
+    }
+  );
+});
+
