e42e9603ff37dcf7d3a3e4d134db350216779c58
Graham Robertson <root@graham-robertson.ca>
2013-11-20 18:47:02 -0500

    Stuuuuuf
    mhmm

--- src/js/ChromeCommunicator.js
+++ src/js/ChromeCommunicator.js
@@ -1,42 +1,84 @@
-(function CommunicatorModule(_chrome) {
-	
-	var ReadyEvent = function ReadyEvent() {
-		
+(function Communicator(_win, _chrome) {
+	var SocketReadyEvent = function SocketReadyEvent(_code) {
+		this.code = _code;
 	};
 	
-	var SentEvent = function SentEvent() {
+	var Connection = function Connection(_myIP, _toIP, _port) {
+		this.inheritFrom = EventDispatcher;
+		this.inheritFrom();
+		delete this.inheritFrom;
+
+		var __self__ = this;
+		var _socketId = -1;
+
+		var __constructor__ = function() {
+	  		_chrome.socket.create('udp', null, onCreate);
+		};
+
+		var onCreate = function(createInfo){
+			_socketId = createInfo.socketId;
+			_chrome.socket.bind(
+				_socketId,
+				_myIP,
+				_port,
+				onBind
+			);
+			_chrome.socket.read(socket, 1024, onRead);
+			_chrome.socket.connect(_socketId, _toIP, _port, onConnect);
+		};
+
+		var onBind = function(result) {
+			__self__.dispatchEvent(Socket.READY, new CReadyEvent(result));
+		};
+
+		var onRead = function(readInfo) {
+			__self__.dispatchEvent(Socket.RECEIVED, readInfo);
+		};
 		
-	};
-	
-	var ReceivedEvent = function ReceivedEvent() {
+		var onConnect = function(result) {
 		
-	};
+		};
+
+		this.send = function(ip) {
+		
+		};
+		
+		this.destroy = function() {
+		
+		};
 	
-	var ChromeCommunicator = window.ChromeCommunicator =
+		__constructor__();
+	};
+	Socket.READY = 0;
+	Socket.SENT = 1;
+	Socket.RECEIVED = 2;
+
+	var ChromeCommunicator = _win.ChromeCommunicator =
 	function ChromeCommunicator() {
 		this.inheritFrom = EventDispatcher;
 		this.inheritFrom();
 		delete this.inheritFrom;
-		
+
 		var __self__ = this;
+		var _ip = null;
 		
-		
-		var __constructor__ = function() {
-			
+		var onNetworkList = function(data) {
+			if (!data[0]) {
+				__self__.dispatchEvent(ChromeCommunicator.IP_NOT_FOUND);
+				return;
+			}
+			_ip = data[0].address;
+			__self__.dispatchEvent(ChromeCommunicator.READY,{ ip: _ip });
 		};
 		
-		this.send = function(ip, data) {
-			
+		this.start = function() {
+			chrome.getNetworkList(onNetworkList);
 		};
 		
-		this.dispose = function() {
-			
+		this.createConnection = function(ip, port) {
+			return new Connection(_ip, ip, port);
 		};
-		
-		__constructor__();
 	};
-	ChromeCommunicator.READY = 0;
-	ChromeCommunicator.SENT = 1;
-	ChromeCommunicator.RECEIVED = 2;
-	
-})(chrome);
+	ChromeCommunicator.IP_NOT_FOUND = 0;
+	ChromeCommunicator.READY = 1;
+})(window, chrome);
