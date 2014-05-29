fa3213a32664a9d785c7dabaadd94354fbae9872
Graham Robertson <root@graham-robertson.ca>
2014-05-28 20:45:43 -0400

    say hello

--- src/js/core/AirChat.js
+++ src/js/core/AirChat.js
@@ -3,17 +3,16 @@ function AirChat(Communicator) {
 	this.inheritFrom();
 	delete this.inheritFrom;
 	
-	var _communicator;
-	
-	function onready(e) {
-		console.log(e);
+	function onready(ip) {
+		this.bonjour = new Bonjour(ip, _communicator);
 	};
 	
 	function onerror() {
 		console.log('Uh oh');
 	};
 	
-	_communicator = new Communicator(onready, onerror);
+	var _communicator = this.communicator = new Communicator(onready, onerror);
+	this.bonjour;
 };
 AirChat.CONTACTS_UPDATE = 'contacts_update';
 AirChat.MESSAGE_RECEIVED = 'message_received';
