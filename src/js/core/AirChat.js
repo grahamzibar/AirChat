4d88cd48c99aa870ed7bcf08c1ab359157ff6604
Graham Robertson <root@graham-robertson.ca>
2014-06-04 20:03:41 -0400

    bonjour changes
    !

--- src/js/core/AirChat.js
+++ src/js/core/AirChat.js
@@ -4,6 +4,7 @@ function AirChat(Communicator) {
 	delete this.inheritFrom;
 	
 	function onready(ip) {
+		console.log('here');
 		this.bonjour = new Bonjour(ip, _communicator);
 	};
 	
@@ -11,8 +12,7 @@ function AirChat(Communicator) {
 		console.log('Uh oh');
 	};
 	
-	var _communicator = this.communicator = new Communicator(onready, onerror);
-	this.bonjour;
+	var _communicator = this.communicator = new Communicator(onready.bind(this), onerror);
 };
 AirChat.CONTACTS_UPDATE = 'contacts_update';
 AirChat.MESSAGE_RECEIVED = 'message_received';
--- src/js/core/Bonjour.js
+++ src/js/core/Bonjour.js
@@ -1,7 +1,5 @@
 window.Bonjour = function Bonjour(_ip, _communicator) {
 	var __self__ = this;
-	
-	var BROADCAST_DELAY = 10000;
 	var TIMEOUT = 60000;
 	
 	var _ipList = new Array();
@@ -10,23 +8,61 @@ window.Bonjour = function Bonjour(_ip, _communicator) {
 	var _s;
 	
 	function __constructor__() {
-		console.log('constructing');
-		setReceptionist();
-		setTimeout(timeoutCheck, TIMEOUT);
-		
 		_ip = new String(_ip);
-		getIps();
-		console.log('Creating Broadcaster');
-		_s = _communicator.createSocket(Socket.UDP, 5554, onCreate);
+		calculateIps();
+		_receptionist = _communicator.createSocket('udp', 5556, onReceptionistCreated);
+		_s = _communicator.createSocket(Socket.UDP, 5554, onBroadcasterCreated);
 	};
 	
-	function onCreate() {
+	// HELPERS
+	function calculateIps() {
+		var pivot = _ip.lastIndexOf('.') + 1;
+		var curBlock = parseInt(_ip.substr(pivot));
+		var baseIp = _ip.substring(0, pivot);
+		for (var i = 0, ipList = _ipList; i <= 255; i++) {
+			if (i != curBlock)
+				ipList.push(baseIp + i);
+		}
+	};
+	
+	function registerIp(receivedIp) {
+		if (!_activeList[receivedIp])
+			_ipList.splice(_ipList.indexOf(receivedIp), 1);
+		_activeList[receivedIp] = getNow();
+	};
+	
+	function getNow() {
+		return Date.now();
+	};
+	
+	// API
+	function bonjour(to) {
+		_s.send(to, 5556, str2ab("Bonjour:" + _ip));
+	};
+	
+	function broadcast() {
+		console.log('Broadcasting!!!!!!');
+		for (var i = 0; i < _ipList.length; i++)
+			bonjour(_ipList[i]);
+	};
+	
+	function keepAlive() {
+		var now = getNow();
+		for (var ip in _activeList) {
+			var time = _activeList[ip];
+			if (now - time >= TIMEOUT)
+				_ipList.push(receivedIp);
+		}
+		setTimeout(timeoutCheck, TIMEOUT); 
+	};
+	
+	// HANDLERS
+	function onBroadcasterCreated() {
 		console.log('Broadcaster Created');
 		_s.addEventListener(window.Socket.RECEIVED, onBroadcasterReceived);
-		broadcast();
 	};
 	
-	function onBroadcasterReceived(receivedEvent) { //maybe change this name
+	function onBroadcasterReceived(receivedEvent) {
     		console.log('Broadcaster Received from ', ab2str(receivedEvent.data));
 		if (receivedEvent.data) {
 			var receivedIp = ab2str(receivedEvent.data).split("Bonjour:");
@@ -45,60 +81,17 @@ window.Bonjour = function Bonjour(_ip, _communicator) {
 		if (receivedEvent.data) {
 			var receivedIp = ab2str(receivedEvent.data).split("Bonjour:");
 			if (receivedIp = receivedIp[1]) {
-				registerIp(receivedIp)
+				registerIp(receivedIp);
 				_receptionist.send(receivedIp, 5554, str2ab("Bonjour:" + _ip));
 			}
 		}
 	};
 	
-	function timeoutCheck() {
-		var now = getNow();
-		// change to array eventually
-		for (var ip in _activeList) {
-			var time = _activeList[ip];
-			if (now - time >= TIMEOUT)
-				_ipList.push(receivedIp);
-		}
-		setTimeout(timeoutCheck, TIMEOUT); 
-	};
-	
-	function getNow() {
-		return Date.now();
-	};
-	
-	function bonjour(to) {
-		_s.send(to, 5556, str2ab("Bonjour:" + _ip));
-	};
-	
-	function registerIp(receivedIp) {
-		if (!_activeList[receivedIp])
-			_ipList.splice(_ipList.indexOf(receivedIp), 1);
-		_activeList[receivedIp] = getNow();
-	};
-	
-	function broadcast() {
-		console.log('Broadcasting!!!!!!');
-		for (var i = 0; i < _ipList.length; i++)
-			bonjour(_ipList[i]);
-		setTimeout(broadcast, BROADCAST_DELAY); // perhaps we need some way to flag this?
-	};
-	
-	function setReceptionist() {
-		console.log('Creating Receptionist');
-		_receptionist = _communicator.createSocket('udp', 5556, onReceptionistCreated);
-	};
-	
-	function getIps() {
-		var pivot = _ip.lastIndexOf('.') + 1;
-		var curBlock = parseInt(_ip.substr(pivot));
-		var baseIp = _ip.substring(0, pivot);
-		for (var i = 0, ipList = _ipList; i <= 255; i++) {
-			if (i != curBlock)
-				ipList.push(baseIp + i);
-		}
-	};
-	
 	this.broadcast = broadcast.bind(this);
+	this.keepAlive = keepAlive.bind(this);
+	this.getActiveList = function() {
+		return _activeList;
+	};
 	
 	__constructor__();
 };
