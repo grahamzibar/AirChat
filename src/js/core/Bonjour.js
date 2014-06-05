5fa09be03bce4fef1e7575ad7e12c1a1429ed5d0
Graham Robertson <root@graham-robertson.ca>
2014-06-05 13:44:32 -0400

    bonjour restructuring
    I feel as if though ALL communication can happen over one socket/port.
    Perhaps we make a Messenger class which acts like a controller: it
    handles all incoming and outgoing messages and redirects those to
    different objects cor handling.  Thoughts?

--- src/js/core/Bonjour.js
+++ src/js/core/Bonjour.js
@@ -5,6 +5,7 @@ window.Bonjour = function Bonjour(_ip, _communicator) {
 	var SALUT = 1;
 	var BYE = 2;
 	var STILL_HERE = 3;
+	var DIT = 4;
 	
 	var PORT = 5554;
 	var TIMEOUT = 60000;
@@ -14,8 +15,7 @@ window.Bonjour = function Bonjour(_ip, _communicator) {
 	var _s;
 	
 	function __constructor__() {
-		_ip = new String(_ip);
-		calculateIps();
+		calculateIps(new String(_ip));
 		_s = _communicator.createSocket(Socket.UDP, PORT, onCreated);
 	};
 	
@@ -26,20 +26,20 @@ window.Bonjour = function Bonjour(_ip, _communicator) {
 		return str2ab(command);
 	};
 	
-	function calculateIps() {
-		var pivot = _ip.lastIndexOf('.') + 1;
-		var curBlock = parseInt(_ip.substr(pivot));
-		var baseIp = _ip.substring(0, pivot);
+	function calculateIps(ip) {
+		var pivot = ip.lastIndexOf('.') + 1;
+		var curBlock = parseInt(ip.substr(pivot));
+		var baseIp = ip.substring(0, pivot);
 		for (var i = 0, ipList = _ipList; i <= 255; i++) {
 			if (i != curBlock)
 				ipList.push(baseIp + i);
 		}
 	};
 	
-	function registerIp(receivedIp) {
-		if (!_activeList[receivedIp])
-			_ipList.splice(_ipList.indexOf(receivedIp), 1);
-		_activeList[receivedIp] = getNow();
+	function registerIp(ip) {
+		if (!_activeList[ip])
+			_ipList.splice(_ipList.indexOf(ip), 1);
+		_activeList[ip] = getNow();
 	};
 	
 	function removeIp(ip) {
@@ -67,15 +67,15 @@ window.Bonjour = function Bonjour(_ip, _communicator) {
 		var now = getNow();
 		for (var ip in _activeList) {
 			var time = _activeList[ip];
-			if (now - time >= TIMEOUT)
+			if (now - time >= TIMEOUT) {
+				console.log(ip, 'est perdu, donc on va le supprimer.');
 				removeIp(ip);
-			else
+			} else
 				_s.send(ip, PORT, createMessage(STILL_HERE));
 		}
 	};
 	
 	function bye() {
-		//_s.removeEventListener();
 		for (var ip in _activeList)
 			_s.send(ip, PORT, createMessage(BYE));
 	};
@@ -89,32 +89,50 @@ window.Bonjour = function Bonjour(_ip, _communicator) {
 		if (receivedEvent.data) {
 			var ip = ab2str(receivedEvent.data).split('|');
 			var command = Number(ip[0]);
+			if (ip.length > 2)
+				var message = ip[2];
 			ip = ip[1];
 			
 			switch (command) {
 				case BONJOUR;
+					console.log(ip, 'a dit <<bonjour>>!');
 					registerIp(ip);
 					_s.send(ip, PORT, createMessage(SALUT));
 					break;
 				case SALUT:
+					console.log(ip, 'a dit <<salut>>!');
 					registerIp(ip);
 					break;
 				case STILL_HERE:
+					//console.log(ip, 'est encore là.');
 					registerIp(ip);
 					break;
 				case BYE:
+					console.log(ip, 'est parti d\'ici.');
 					removeIp(ip);
 					break;
+				case DIT:
+					console.log(ip, ':', message);
+					break;
 				default:
-					console.log('WTF?');
+					console.log('Quoi? Ce n\'est pas possible. On doit debug ça.');
 					break;
 			}
 		}
 	};
 	
-	this.broadcast = broadcast.bind(this);
-	this.keepAlive = keepAlive.bind(this);
-	this.bye = bye.bind(this);
+	
+	this.sayHello = broadcast.bind(this);
+	this.poke = keepAlive.bind(this);
+	this.sayGoodBye = bye.bind(this);
+	this.send = function(to, message) {
+		command = DIT;
+		command += '|';
+		command += _ip;
+		command += '|';
+		command += message;
+		_s.send(to, PORT, str2ab(command));
+	};
 	this.getActiveList = function() {
 		return _activeList;
 	};
